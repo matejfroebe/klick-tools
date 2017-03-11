0fa09e1439535fb4e8b2b6e601379c339b68f6f2
Matej Fröbe <matej.froebe@gmail.com>
2017-03-11 22:11:26 +0100

    new tool for tempo map templates

---
+++ klick-template.py
@@ -0,0 +1,23 @@
+
+import argparse
+
+import klick
+
+
+parser = argparse.ArgumentParser(description='Run klick with a template file for tempo map.')
+parser.add_argument('BPM', type=int, nargs='+', help='tempo (BPM)')
+parser.add_argument('-f', '--filename', help='filename')
+
+
+args = parser.parse_args()
+BPM = args.BPM[0]
+
+with open("template.klick") as f:
+    linesIn = f.readlines()
+
+linesOut = [line.replace("$T", str(BPM)) for line in linesIn]
+
+print linesOut
+
+
+klick.runKlick(linesOut)
