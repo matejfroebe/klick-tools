d0f0454016651e702619ff2ff6493dbee3d7740a
Matej Fröbe <matej.froebe@gmail.com>
2017-08-06 10:02:50 +0200

    filename as command line parameter

--- klick-template.py
+++ klick-template.py
@@ -12,7 +12,7 @@ parser.add_argument('-f', '--filename', help='filename')
 args = parser.parse_args()
 BPM = args.BPM[0]
 
-with open("template.klick") as f:
+with open(args.filename) as f:
     linesIn = f.readlines()
 
 linesOut = [line.replace("$T", str(BPM)) for line in linesIn]
